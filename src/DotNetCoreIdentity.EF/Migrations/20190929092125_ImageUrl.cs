818a7f2af495e37019a8ed3f36e38d6d81b3845b
kadirsevik <ksevik9@hotmail.com>
2019-12-11 14:27:15 +0300

    Initial
    Initial Create

---
+++ src/DotNetCoreIdentity.EF/Migrations/20190929092125_ImageUrl.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCoreIdentity.EF.Migrations
+{
+    public partial class ImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Posts");
+        }
+    }
+}
